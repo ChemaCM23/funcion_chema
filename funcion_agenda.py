018206a34f00836347fa2c11c55bb71e4749dbcc
ChemaCM23 <93979793+ChemaCM23@users.noreply.github.com>
2022-03-08 13:03:07 +0100

    Update funcion_agenda.py

--- funcion_agenda.py
+++ funcion_agenda.py
@@ -1,7 +1,5 @@
-agenda = {}
-#Función añadir o modificar#
-def añadirModificarJcaballero():
-    nombre = input("Nombre del contacto: ")
+#Este es el código de Chema de la función de añadir modificar# 
+def añadirModificarJcaballero(nombre, agenda):
     if nombre in agenda:
         print("%s es un contacto que ya existe, su número de teléfono es %s" % (nombre,agenda[nombre]))
         selec = input("Pulsa la tecla 's' si quiere modificarlo. Cualquier otra tecla para continuar.")
